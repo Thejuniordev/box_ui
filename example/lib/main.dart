f7a0eb058cf05122d4448581c9cb13836bf68d34
Junior Alves <junioralves.webdeveloper@gmail.com>
2021-09-15 12:08:42 -0300

    Resolve path import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,22 +1,23 @@
-// import 'package:box_ui/box_ui.dart';
-// import 'package:flutter/material.dart';
+import 'package:box_ui/box_ui.dart';
+import 'package:example/src/shared/app_colors.dart';
+import 'package:flutter/material.dart';
 
-// import 'example_view.dart';
+import 'example_view.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() {
+  runApp(MyApp());
+}
 
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primaryColor: kcPrimaryColor,
-//       ),
-//       home: ExampleView(),
-//     );
-//   }
-// }
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: kcPrimaryColor,
+      ),
+      home: ExampleView(),
+    );
+  }
+}
